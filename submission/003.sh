f1a89d763cde4b24785b4fe853df7990abeb46ff
Frederico <fredericofabricio5@gmail.com>
2024-11-09 12:21:25 -0300

    trocado para o bin do bitcoin-cli para ser reconhecido no PATH do sistema

--- submission/003.sh
+++ submission/003.sh
@@ -1,4 +1,4 @@
 # How many new outputs were created by block 123,456?
-blockTargetHash=$(./bitcoin-cli getblockhash 123456)
-vOutTx=$(./bitcoin-cli getblock $blockTargetHash 2 | jq '[.tx[].vout | length] | add')
+blockTargetHash=$(bitcoin-cli getblockhash 123456)
+vOutTx=$(bitcoin-cli getblock $blockTargetHash 2 | jq '[.tx[].vout | length] | add')
 echo "existem $vOutTx outputs no bloco 123456"
