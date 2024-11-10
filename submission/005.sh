f8923894ac4f0eb3d414cc58c53e630dd79c1a18
Frederico <fredericofabricio5@gmail.com>
2024-11-10 12:33:18 -0300

    echo no submission 5

--- submission/005.sh
+++ submission/005.sh
@@ -8,4 +8,4 @@ key3=$(bitcoin-cli getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd
 
 parser="[\"$key0\", \"$key1\", \"$key2\", \"$key3\"]"
 
-bitcoin-cli createmultisig 1 "$parser"
+echo $(bitcoin-cli createmultisig 1 "$parser")
