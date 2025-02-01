5d0d18b86b4222245b2de363c7396035bffd968c
Frederico <fredericofabricio5@gmail.com>
2025-02-01 17:37:33 -0300

    008.sh changes

--- submission/008.sh
+++ submission/008.sh
@@ -2,11 +2,10 @@
 
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
-tx=$(bitcoin-cli getrawtransaction $txid true)
+tx=$(./bitcoin-cli getrawtransaction $txid true)
 
-publickey=$(echo "$tx" | jq -r '.vin[0].txinwitness[2]')
+publickey=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
 
-echo $publickey
+pubkey=$(echo $publickey | grep -oP '2102\K[0-9a-fA-F]{66}')
 
-
-
+echo "$pubkey"
