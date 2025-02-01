b71038247043758c0b2846a45b4a343df4129eda
Frederico <fredericofabricio5@gmail.com>
2025-02-01 17:24:11 -0300

    added [006..008].sh submission

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,21 @@
-# Only one single output remains unspent from block 123,321. What address was it sent to?
+#!/bin/bash
+
+blockHash=$(bitcoin-cli getblockhash 123321)
+infoBlock=$(bitcoin-cli getblock $blockHash | jq -r '.tx[]')
+addr=""
+
+for txid in $infoBlock; do
+  
+  tx=$(bitcoin-cli getrawtransaction $txid true)
+  vout_count=$(echo "$tx" | jq '.vout | length')
+  for (( i=0; i<$vout_count; i++ )); do
+    address=$(bitcoin-cli gettxout $txid $i)
+    if [[ -z "$address" ]]; then
+      :
+    else
+      echo $address | jq -r '.scriptPubKey.address'
+      exit 0
+    fi
+
+  done
+done
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,12 @@
-# Which public key signed input 0 in this tx:
-#   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+#!/bin/bash
+
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+tx=$(bitcoin-cli getrawtransaction $txid true)
+
+publickey=$(echo "$tx" | jq -r '.vin[0].txinwitness[2]')
+
+echo $publickey
+
+
+
