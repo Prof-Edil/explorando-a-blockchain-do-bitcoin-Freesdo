93a811f5782db2b36e3c3d7bc4b57d9adf140b32
Frederico <fredericofabricio5@gmail.com>
2025-02-02 17:16:53 -0300

    eu não faço essas mensagens de commit em produção, eu juro

--- submission/008.sh
+++ submission/008.sh
@@ -7,12 +7,12 @@ txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
 tx=$(bitcoin-cli getrawtransaction $txid true)
 
-publickey=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
+publickeyencode=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
 
 
 
-pubkey1=$(echo "$publickey" | grep -oP '2102\K[0-9a-fA-F]{66}' | sed 's/..$//')
-pubkey2=$(echo "$publickey" | grep -oP '2102\K[0-9a-fA-F]{66}' | tail -n 1 | sed 's/..$//')
+pubkey1=$(echo "$publickeyencode" | grep -oP '2102\K[0-9a-fA-F]{66}' | sed 's/..$//' | head -n 1)
+pubkey2=$(echo "$publickeyencode" | grep -oP '2102\K[0-9a-fA-F]{66}' | tail -n 1 | sed 's/..$//')
 
 
 echo "02$pubkey1"
