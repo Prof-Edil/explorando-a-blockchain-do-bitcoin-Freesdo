85e30f82d6b144926033178dc5d65304599f26e3
Frederico <fredericofabricio5@gmail.com>
2025-02-02 17:10:14 -0300

    teste

--- submission/008.sh
+++ submission/008.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Which public key signed input 0 in this tx:
+#   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
 tx=$(bitcoin-cli getrawtransaction $txid true)
 
-publickey=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
+publickeyencode=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
 
-echo $publickey
+pubkey1=$(echo "$publickeyencode" | grep -oP '2102\K[0-9a-fA-F]{66}' | sed 's/..$//')
+pubkey2=$(echo "$publickeyencode" | grep -oP '2102\K[0-9a-fA-F]{66}' | tail -n 1 | sed 's/..$//')
+
+
+echo "02$pubkey2"
 
